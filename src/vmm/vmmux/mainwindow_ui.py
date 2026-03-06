a4cdb337fc7e2260d9857816dc5228208b799001
Roy <roynielsen17@gmail.com>
2026-03-05 19:29:51 -0700

    cleaning up ui directory

--- src/vmm/vmmux/mainwindow_ui.py
+++ src/vmm/vmmux/mainwindow_ui.py
@@ -141,7 +141,7 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
 
-        self.stackedWidget.setCurrentIndex(3)
+        self.stackedWidget.setCurrentIndex(0)
 
 
         QMetaObject.connectSlotsByName(MainWindow)
